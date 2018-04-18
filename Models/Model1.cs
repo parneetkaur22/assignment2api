e8cf51b4111faea5dac7e19b7c326dc3a5c284ea
parneetkaur22 <31783681+parneetkaur22@users.noreply.github.com>
2018-04-18 19:21:40 -0400

    second commit

---
+++ Models/Model1.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment2API.Models
+{
+    public class Model1 : DbContext
+    {
+        // constructor
+
+        public Model1(DbContextOptions<Model1>options) : base(options)
+        {
+
+
+        }
+
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Assignment2API.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Assignment2API
 {
@@ -24,6 +26,11 @@ namespace Assignment2API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            // db conn 
+            var db = Configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<Model1>(options => options.UseSqlServer(db));
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
